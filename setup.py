9f8e567383d6ad8094e199801da74fa2049b41a3
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-15 13:52:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="Helpo",
+    name="PyroHelpo",
     version="1.0.0",
     author="Vishal & Siyu",
     author_email="vishalborse2885@gmail.com",
