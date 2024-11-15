b31f159f841585ab4a8098b01692cfb95598d3f7
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-15 14:03:31 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from .core import Helpo
-from .decorators import module_help
+from core import Helpo
+from decorators import module_help
 
-__all__ = ["PyroHelpo", "module_help"]
+__all__ = ["Helpo", "module_help"]
+
+__version__ = "1.0.0"
