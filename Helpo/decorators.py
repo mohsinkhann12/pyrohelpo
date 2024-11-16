b36f85b37760ba9511ce0610b7df249dac28e6a5
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-16 09:14:55 +0530

    Delete Helpo/decorators.py

--- Helpo/decorators.py
+++
@@ -1,7 +0,0 @@
-def module_help(module_name: str):
-    def decorator(func):
-        func.__MODULE__ = module_name
-        return func
-    return decorator
-
-print("Helpo decorators module loaded")
