ed9ffcdcc9842f8942ce16ceb40f0311c2b31a20
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-16 09:14:42 +0530

    Update __init__.py

--- Helpo/__init__.py
+++ Helpo/__init__.py
@@ -1,11 +1,5 @@
 from Helpo.core import Helpo
-from Helpo.decorators import module_help
 
-async def deep_linking(chat_id: int, page: int = 1, message_id: int = None, instance: Helpo = None):
-    if instance is None:
-        raise ValueError("An instance of Helpo must be provided.")
-    return await instance.show_help_menu(chat_id, page, message_id)
+__all__ = ["Helpo"]
 
-__all__ = ["Helpo", "module_help"]
-
-__version__ = "1.0.0"
+__version__ = "1.9.3"
